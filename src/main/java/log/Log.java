f2d84e1cf67c1ea8839375ddd5595798a1bedab4
Juliana Giacomo <ju.giacomo@gmail.com>
2017-10-08 20:13:46 -0300

    cadastro do log

--- src/main/java/log/Log.java
+++ src/main/java/log/Log.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -119,5 +121,79 @@ public class Log {
         log.put("registro", registro);
 
         return log;
+    }
+
+    public String check(){
+        boolean check = this.produto == null || this.produto.equals("defensoria") || this.produto.equals("OAB");
+        if(!check)
+            return "Produto inválido";
+
+        if(usuario == null || url == null || metodo == null)
+            return  "Dados não informados";
+
+        if(!this.metodos().contains(metodo))
+            return "Método inválido";
+
+        if(status < 100 && status > 600)
+            return "Status inválido";
+
+        check = this.cliente == null || this.clientes().contains(this.cliente);
+        if(!check)
+            return "Cliente inválido";
+
+        check = this.categoria == null || this.categoria().contains(this.categoria);
+        if(!check)
+            return "Categoria inválida";
+
+        return null;
+    }
+
+    private List clientes(){
+        List<String> clientes = new ArrayList<String>();
+        clientes.add("Alagoas");
+        clientes.add("Sergipe");
+        clientes.add("Bahia");
+        clientes.add("Ceará");
+        clientes.add("Pernambuco");
+        clientes.add("Maranhão");
+        clientes.add("Piauí");
+        clientes.add("Rio Grande do Norte");
+        clientes.add("Paraíba");
+
+        return clientes;
+    }
+
+    private List categoria(){
+        List<String> categoria = new ArrayList<String>();
+        categoria.add("erro");
+        categoria.add("permissão");
+        categoria.add("autenticação");
+        categoria.add("requisição");
+        categoria.add("conexão");
+
+        return categoria;
+    }
+
+    private List metodos (){
+        List<String> metodos = new ArrayList<String>();
+        metodos.add("POST");
+        metodos.add("GET");
+        metodos.add("PUT");
+        metodos.add("DELETE");
+
+        return metodos;
+    }
+
+    public static LocalDateTime formatarPrazo(String prazo) {
+        String pattern = "((0[1-9])|([1-2][0-9])|(3[0-1]))\\/((0[1-9])|(1[0-2]))\\/\\d{4}((0[0-9])|(1[0-9])|(2[0-3])):((0[0-9])|([1-5][0-9]))";
+        if(prazo.matches(pattern)){
+            int dia = Integer.parseInt(prazo.substring(0, 2));
+            int mes = Integer.parseInt(prazo.substring(3, 5));
+            int ano = Integer.parseInt(prazo.substring(6, 10));
+            int hora = Integer.parseInt(prazo.substring(10, 12));
+            int minuto = Integer.parseInt(prazo.substring(13, 15));
+            return LocalDateTime.of(ano, mes, dia, hora, minuto);
+        }
+        return null;
     }
 }
--- src/main/java/log/LogController.java
+++ src/main/java/log/LogController.java
@@ -1,21 +1,49 @@
 package log;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
-@RequestMapping(path="/api")
+@RequestMapping(path="/api/log")
 public class LogController {
 
     @Autowired
     private LogRepository logRepository;
 
-    @GetMapping(path="/log")
+    @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody
-    Iterable<Log> getLogs() {
-        return logRepository.findAll();
+    ResponseEntity getLogs() {
+        Iterable<Log> logs;
+        List<Map<String, Object>> json = new ArrayList<>();
+        logs = logRepository.findAll();
+
+        for(Log log : logs){
+            json.add(log.getJson());
+        }
+        return ResponseEntity.ok(json);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity createLogs(@RequestBody Log log) {
+
+        log.setDataHora(LocalDateTime.now());
+
+        String validate =  log.check();
+        if(validate != null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validate);
+
+        logRepository.save(log);
+
+
+        return ResponseEntity.ok(log.getJson());
     }
 }
